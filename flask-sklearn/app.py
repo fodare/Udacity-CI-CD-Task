953f9f0778b2a270113b81a23290d5e080732b65
fodare <folorunsodamilare007@hotmail.com>
2023-08-29 11:05:18 +0200

    Adding new test for locust predition test.

--- flask-sklearn/app.py
+++ flask-sklearn/app.py
@@ -33,9 +33,9 @@ def predict():
     # Performs an sklearn prediction
     try:
         # Load pretrained model as clf. Try any one model.
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        clf = joblib.load(
-            "./Housing_price_model/StochasticGradientDescent.joblib")
+        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        # clf = joblib.load(
+        #     "./Housing_price_model/StochasticGradientDescent.joblib")
         # clf = joblib.load(
         #     "./Housing_price_model/GradientBoostingRegressor.joblib")
     except:
--- flask-sklearn/locustfile.py
+++ flask-sklearn/locustfile.py
@@ -1,13 +1,33 @@
 from locust import HttpUser, task
+import requests
 
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
-        base_url = "https://testudacityapp.azurewebsites.net/"
+        base_url = "https://udacitytestmlappservice.azurewebsites.net"
         self.client.get(f"{base_url}")
 
     @task
-    def hello_world(self):
-        base_url = "https://testudacityapp.azurewebsites.net/predict"
-        self.client.get(f"{base_url}")
+    def predict(self):
+        base_url = "https://udacitytestmlappservice.azurewebsites.net:443/predict"
+        self.client.post(f"{base_url}", json={
+            "CHAS": {
+                "0": 0
+            },
+            "RM": {
+                "0": 6.575
+            },
+            "TAX": {
+                "0": 296.0
+            },
+            "PTRATIO": {
+                "0": 15.3
+            },
+            "B": {
+                "0": 396.9
+            },
+            "LSTAT": {
+                "0": 4.98
+            }
+        })
